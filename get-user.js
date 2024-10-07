dc7dc95d6e422515a6e6d8d2010b882e1d84abdc
Mansoor Hussain <hussainmansoor876@gmail.com>
2024-10-08 03:34:00 +0500

    updated

--- get-user.js
+++ get-user.js
@@ -1,16 +1,43 @@
+let apiUrl = `https://tyler-2.13357.wl.simvoly.com/api/site`
+let memberRoute = `members/search-by-email`
+let apiToken = `w7e7ae734df7c4d56b009d7c6e530befc`
+
 document.addEventListener('DOMContentLoaded', function () {
-    console.log('Hello')
     const emailInput = document.querySelector('input[name="email"]');
+    const form = document.getElementById('emailForm');
 
     // Check if there's a saved value in localStorage and set it as the input value
-    const savedEmail = localStorage.getItem('email')
+    const savedEmail = localStorage.getItem('email');
     if (savedEmail) {
         emailInput.value = savedEmail;
     }
 
     // Add an event listener to save the value in localStorage whenever it changes
     emailInput.addEventListener('input', function (event) {
-        const emailValue = event.target.value;
-        localStorage.setItem('email', emailValue); // Save to localStorage
+        const emailValue = event.target.value
+        localStorage.setItem('email', emailValue) // Save to localStorage
+    })
+
+    // Add event listener for form submission
+    form.addEventListener('submit', function (event) {
+        event.preventDefault(); // Prevent the form from submitting the traditional way
+
+        const emailValue = emailInput.value;
+
+        // Send the email to the backend
+        fetch(`${apiUrl}/${memberRoute}?${emailValue}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${apiToken}`
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Email sent:', data)
+            })
+            .catch(error => {
+                console.error('Error sending email:', error)
+            });
     });
 });
