8d560b691f9c339f3aa0da5518bd82d9d84c045f
Mansoor Hussain <hussainmansoor876@gmail.com>
2024-10-12 04:11:45 +0500

    Update app.js

--- app.js
+++ app.js
@@ -308,7 +308,7 @@ console.log('pathname', routeURL)
 
 if (category) {
     document.addEventListener('DOMContentLoaded', function () {
-        const productLink = document.querySelector('#store-widget-1714582254991 > div > div > div')
+        const productLink = document.querySelectorAll('.widget-row')
 
         console.log('productLink updated', productLink)
         console.log('productLink updated vv', document.body)
