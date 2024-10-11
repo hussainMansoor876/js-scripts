499763c2705842424c2f0a3bc0b45b3a0647ec63
Mansoor Hussain <hussainmansoor876@gmail.com>
2024-10-12 04:33:36 +0500

    Update app.js

--- app.js
+++ app.js
@@ -316,7 +316,7 @@ if (category && category?.id) {
 
         console.log('productLink updated div 1', productLink[0])
         console.log('productLink updated div 2', productLink[0].children)
-        console.log('productLink updated div', productLink[0]?.[0]?.[0])
+        console.log('productLink updated div', productLink[0]?.children?.[0]?.children)
 
     })
 }
