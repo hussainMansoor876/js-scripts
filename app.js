84a44f0d40e0ed0c6b69695d9957c2ead88e27f8
Mansoor Hussain <hussainmansoor876@gmail.com>
2024-10-12 07:07:11 +0500

    Update app.js

--- app.js
+++ app.js
@@ -337,7 +337,8 @@ function handleNewChild(parentDiv) {
     localStorage.setItem('percentage', JSON.stringify(percentage))
 
     Array.from(parentDiv.children).forEach(child => {
-        console.log('child?.children', child?.firstElementChild?.firstElementChild?.firstElementChild?.firstElementChild)
+        let events = child?.firstElementChild?.firstElementChild?.firstElementChild?.firstElementChild
+        console.log('events', events)
         let prices = child?.children?.[1]?.children?.[0]?.children?.[0]?.children?.[2]?.firstElementChild?.childNodes
         console.log('prices', prices)
         let textValue = prices?.[1].innerHTML?.split('$')?.[0] || ``
