572a1d6758506b13b5aa2d0e06596d2b00a2267d
Mansoor Hussain <hussainmansoor876@gmail.com>
2024-10-12 05:13:02 +0500

    Update get-user.js

--- get-user.js
+++ get-user.js
@@ -87,7 +87,8 @@ document.addEventListener('DOMContentLoaded', function () {
         emailInput.value = savedEmail;
     }
 
-    loginButton.addEventListener('click', () => {
+    loginButton.addEventListener('click', (event) => {
+        event.preventDefault()
         fetchUserByEmail(emailInput?.value)
     })
 
