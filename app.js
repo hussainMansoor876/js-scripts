f391354fac49a8c4f6464d7221589b3480f5e1a3
Mansoor Hussain <hussainmansoor876@gmail.com>
2024-10-13 05:19:45 +0500

    Update app.js

--- app.js
+++ app.js
@@ -332,7 +332,6 @@ function handleNewChild(parentDiv) {
 
     let firstProduct = parentDiv.firstElementChild
     let prices = firstProduct.children?.[1]?.children?.[0]?.children?.[0]?.children?.[2]?.firstElementChild?.childNodes
-    console.log('prices', prices)
     let newPrice = parseFloat(prices?.[0]?.nodeValue?.split('$')?.slice(-1,)[0])
     let oldPrice = parseFloat(prices?.[1]?.innerHTML?.split('$')?.slice(-1,)[0])
     percentage = calculateDiscountPercentage(oldPrice, newPrice) / 100
