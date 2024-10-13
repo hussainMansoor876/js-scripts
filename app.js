d218e4a69dc5a154431eb4da3a32f0740c6630e9
Mansoor Hussain <hussainmansoor876@gmail.com>
2024-10-13 05:13:36 +0500

    Update app.js

--- app.js
+++ app.js
@@ -340,7 +340,7 @@ function handleNewChild(parentDiv) {
     Array.from(parentDiv.children).forEach(child => {
         let events = child?.children?.[0]?.children?.[0]?.children?.[0]?.children?.[0]
         console.log('events.children', events)
-        console.log('events.children', events.children)
+        console.log('events.children', child.querySelectorAll('a.highlightColor'))
         events.addEventListener('mouseover', (event) => {
             event.preventDefault()
             console.log('Hello')
