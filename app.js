d3c33009ee205028d1754e35df4501e0b73e928c
Mansoor Hussain <hussainmansoor876@gmail.com>
2024-10-14 02:50:44 +0500

    Update app.js

--- app.js
+++ app.js
@@ -342,6 +342,7 @@ const handleNewChild = (parentDiv) => {
         let events = child.querySelector('div.quick-buttons-wrapper.flex.align-center.justify-center')
         let title = child.querySelector('a.highlightColor')?.innerHTML
         var isMouseOver = false
+        events.onclick = ()=> console.log('Click')
         events.addEventListener('mouseover', async () => {
             if (title && !isMouseOver) {
                 let data = await sendRequest(`${apiUrl}/${productRoute}`, 'GET', null, [{ category_id: category?.id }, { limit: 50 }, { title }])
