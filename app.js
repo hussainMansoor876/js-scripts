48337a5ba8c257a43be4797abe6cf6848a09ba16
hussainmansoor876 <hussainmansoor876@gmail.com>
2024-12-20 01:20:49 +0500

    Update app.js

--- app.js
+++ app.js
@@ -556,7 +556,7 @@ document.addEventListener('DOMContentLoaded', async function () {
             if (cartDiv?.children?.length) {
                 console.log('cartDiv', cartDiv.children)
                 for (var v of cartDiv?.children) {
-                    console.log('v', v.querySelector('product-info'))
+                    console.log('v', v)
                 }
             }
 
