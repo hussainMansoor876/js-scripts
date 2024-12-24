3ec3021f0ca58de786e6ff879704bf915c937a1b
hussainmansoor876 <hussainmansoor876@gmail.com>
2024-12-24 23:27:07 +0500

    Update app.js

--- app.js
+++ app.js
@@ -558,7 +558,7 @@ try {
         if (currentTime - lastActivity > oneHour) {
             if (savedEmail?.length) {
                 localStorage.removeItem('email') // Remove email from local storage
-                console.log('Email removed from local storage due to inactivity 1.')
+                console.log('Email removed from local storage due to inactivity.')
 
                 window.location.reload()
             }
@@ -573,7 +573,7 @@ try {
             localStorage.removeItem('email') // Remove email from local storage
             console.log('Email removed from local storage due to inactivity.')
 
-            // window.location.reload()
+            window.location.reload()
         }
         console.log('No lastActivity found in localStorage.')
     }
