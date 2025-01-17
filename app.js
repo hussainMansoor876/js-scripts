68a457643c521767bc7a297b34824c727ab74de2
hussainmansoor876 <hussainmansoor876@gmail.com>
2025-01-17 20:34:48 +0500

    Update app.js

--- app.js
+++ app.js
@@ -784,7 +784,7 @@ const validateSearch = async () => {
 
             // divData.appendChild(newElement)
 
-            divData.innerHTML = htmlData
+            // divData.innerHTML = htmlData
         }
     }
     catch (e) {
